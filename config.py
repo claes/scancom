1d33c41da74dc625edaea5c2def0f91d5e5707a8
Claes Holmerson <user@server>
2012-11-14 00:24:49 +0100

    config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587
+ 
+EMAIL_USER = 'user@example.com'
+EMAIL_PASSWORD = 'this is your SMTP / email password'
+
+TEST_SENDER = 'test@example.com'
+TEST_RECEIVER = 'test@example.com'
+
+OCR_LANGUAGE = 'en'
+
+ 
